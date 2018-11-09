3b1760205cd33b7ad54e53c37206798c2787215a
Andreas Ots <qrpth@qrpth.eu>
2018-11-09 19:54:26 +0200

    Revert "autotopic: add 'approximately'"
    This reverts commit ac259522acc40487515b374e8a05e1aa27145d89.

--- src/autotopic.rs
+++ src/autotopic.rs
@@ -39,7 +39,7 @@ impl<'a> fmt::Display for ShortDisplay<'a> {
         f.write_str(&self.event.summary)?;
 
         if let Some(ref location) = self.event.location {
-            write!(f, " ({})", location)?;
+            write!(f, " ({})", location);
         }
 
         if let Some(ref desc) = self.event.description {
@@ -261,7 +261,7 @@ impl Autotopic {
                     money_raised.separated_string_with_fixed_place(2)
                 ));
                 messages.push(format!(
-                    "Approximately {} of {}+ hours bussed.",
+                    "{} of {}+ hours bussed.",
                     (now - start).num_hours(),
                     total_hours
                 ));
