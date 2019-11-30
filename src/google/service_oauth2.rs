b835da762dc91270313d18712a2466a3a541a014
Andreas Ots <qrpth@qrpth.eu>
2019-12-01 01:23:11 +0200

    breaking change in jsonwebtoken v7: `Key` is gone but it now uses PEM instead of DER

--- src/google/service_oauth2.rs
+++ src/google/service_oauth2.rs
@@ -1,77 +1,21 @@
 //! Creating OAuth2 bearer tokens for Google service accounts
 
-use base64_stream::FromBase64Writer;
 use chrono::{DateTime, Duration, TimeZone, Utc};
 use failure::{Error, ResultExt};
 use futures::compat::Future01CompatExt;
 use futures::lock::Mutex;
-use jsonwebtoken::{Algorithm, Header, Key};
+use jsonwebtoken::{Algorithm, Header};
 use reqwest::r#async::Client;
-use serde::de::Unexpected;
-use serde::{Deserialize, Deserializer, Serialize};
-use slog_scope::error;
-use std::borrow::Cow;
-use std::io::Write;
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use tokio::fs::File;
 
 const TOKEN_URI: &str = "https://www.googleapis.com/oauth2/v4/token";
 
-fn pem_to_der<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    use serde::de::Error;
-
-    let pem = Cow::<str>::deserialize(deserializer)?;
-    let mut output = vec![];
-    {
-        let mut writer = FromBase64Writer::new(&mut output);
-
-        let mut iter = pem.lines().enumerate().peekable();
-        while let Some((i, line)) = iter.next() {
-            if i == 0 {
-                if line != "-----BEGIN PRIVATE KEY-----" {
-                    return Err(Error::invalid_value(
-                        Unexpected::Str(&pem),
-                        &"a valid PEM-encoded PKCS#8-encoded private key: header is incorrect",
-                    ));
-                }
-                continue;
-            } else if iter.peek().is_none() {
-                if line != "-----END PRIVATE KEY-----" {
-                    return Err(Error::invalid_value(
-                        Unexpected::Str(&pem),
-                        &"a valid PEM-encoded PKCS#8-encoded private key: footer is incorrect",
-                    ));
-                }
-                continue;
-            }
-            if let Err(err) = writer.write_all(line.as_bytes()) {
-                error!("Failed to decode the private key"; "error" => ?err);
-                return Err(Error::invalid_value(
-                    Unexpected::Str(&pem),
-                    &"a valid PEM-encoded PKCS#8-encoded private key: content is incorrect",
-                ));
-            }
-        }
-        if let Err(err) = writer.flush() {
-            error!("Failed to decode the private key"; "error" => ?err);
-            return Err(Error::invalid_value(
-                Unexpected::Str(&pem),
-                &"a valid PEM-encoded PKCS#8-encoded private key: content is incorrect",
-            ));
-        }
-    }
-
-    Ok(output)
-}
-
 /// Type of a service account key JSON. There are more fields but we're only interested in these.
 #[derive(Deserialize)]
 struct ServiceAccountKey {
-    #[serde(deserialize_with = "pem_to_der")]
-    private_key: Vec<u8>,
+    private_key: String,
     client_email: String,
 }
 
@@ -141,7 +85,7 @@ impl ServiceAccount {
                     iat: now.timestamp(),
                     exp: (now + Duration::seconds(3600)).timestamp(),
                 },
-                Key::Pkcs8(&key.private_key),
+                key.private_key.as_bytes(),
             )
             .context("failed to create a JWT token")?;
 
