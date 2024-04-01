8b432dd8bfa976c56c65efa8dfb80583d27f1105
Andreas Ots <qrpth@qrpth.eu>
2024-04-01 03:29:28 +0300

    systemd: implement sd_notify(3) ourselves

--- src/systemd.rs
+++ src/systemd.rs
@@ -1,45 +1,50 @@
-use std::ffi::{c_char, c_int, CString};
+use std::os::linux::net::SocketAddrExt;
+use std::os::unix::ffi::OsStrExt;
+use std::os::unix::net::SocketAddr;
 
 use anyhow::{Context, Error};
-use libloading::Library;
+use tokio::net::UnixDatagram;
 
 pub struct Notify {
-    libsystemd: Library,
+    socket: UnixDatagram,
 }
 
 impl Notify {
     pub fn new() -> Result<Self, Error> {
-        let libsystemd = unsafe {
-            Library::new(libloading::library_filename("systemd"))
-                .context("failed to load libsystemd")?
+        let socket_name = std::env::var_os("NOTIFY_SOCKET").context("$NOTIFY_SOCKET is not set")?;
+
+        let addr = if socket_name.as_bytes().starts_with(b"/") {
+            SocketAddr::from_pathname(&socket_name)
+                .context("failed to construct the socket address from path")?
+        } else if socket_name.as_bytes().starts_with(b"@") {
+            SocketAddr::from_abstract_name(&socket_name.as_bytes()[1..])
+                .context("failed to construct the socket address from abstract name")?
+        } else {
+            anyhow::bail!("unsupported socket {socket_name:?}")
         };
 
-        Ok(Self { libsystemd })
+        let std_socket = std::os::unix::net::UnixDatagram::unbound()
+            .context("failed to create a Unix datagram socket")?;
+        std_socket.connect_addr(&addr).with_context(|| format!("failed to connect to {addr:?}"))?;
+        std_socket.set_nonblocking(true).context("failed to set the socket to non-blocking")?;
+
+        let socket = UnixDatagram::from_std(std_socket)
+            .context("failed to convert the socket to a Tokio socket")?;
+
+        Ok(Self { socket })
     }
 
-    fn notify(&self, state: &str) -> Result<bool, Error> {
-        let state = CString::new(state).context("failed to convert the state")?;
-        unsafe {
-            let sd_notify = self
-                .libsystemd
-                .get::<unsafe extern "C" fn(c_int, *const c_char) -> c_int>(b"sd_notify")
-                .context("failed to find `sd_notify` in libsystemd")?;
-            match sd_notify(0, state.as_ptr()) {
-                error if error < 0 => Err(Error::from(std::io::Error::from_raw_os_error(-error))
-                    .context("`sd_notify(3)` failed")),
-                0 => Ok(false),
-                _ => Ok(true),
-            }
-        }
-    }
+    async fn notify(&self, state: &str) -> Result<(), Error> {
+        self.socket.send(state.as_bytes()).await.context("failed to send notification")?;
 
-    pub fn ready(&self) -> Result<(), Error> {
-        self.notify("READY=1")?;
         Ok(())
     }
 
-    pub fn feed_watchdog(&self) -> Result<(), Error> {
-        self.notify("WATCHDOG=1")?;
-        Ok(())
+    pub async fn ready(&self) -> Result<(), Error> {
+        self.notify("READY=1").await
+    }
+
+    pub async fn feed_watchdog(&self) -> Result<(), Error> {
+        self.notify("WATCHDOG=1").await
     }
 }
